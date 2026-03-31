608629cfd65d9dac4977485abf36a51d3e2c526b
Sheldonide <navarrosamueljr@gmail.com>
2026-03-31 15:46:39 -0700

    LiveShare works

--- JoinTest.java
+++ JoinTest.java
@@ -1,6 +1,15 @@
 public class JoinTest{
 
+//Install the LiveShare extension to code simultaneously with another user.
+
+
+//Most recent LiveShare:
 //https://prod.liveshare.vsengsaas.visualstudio.com/join?F6B3959029AD03CECECF121BF333581B73B8
 
+public static void main(String[] args){
+System.out.println("Sheldon coded this");
+
+System.out.println("Toby coded this.");
+}
 
 }
